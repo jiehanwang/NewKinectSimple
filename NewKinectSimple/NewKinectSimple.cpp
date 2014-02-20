9edd2a7e3a92fa821ddcf6c93bd2e3ecef6b6511
WHJLab-PC-i7 <jiehanwang@126.com>
2014-02-20 14:31:58 +0800

    Depth added
    Depth added

--- NewKinectSimple/NewKinectSimple.cpp
+++ NewKinectSimple/NewKinectSimple.cpp
@@ -11,7 +11,7 @@ using namespace std;
 IKinectSensor*          m_pKinectSensor;
 
 // Depth reader
-//IDepthFrameReader*      m_pDepthFrameReader;
+IDepthFrameReader*      m_pDepthFrameReader(NULL);
 
 // Color reader
 IColorFrameReader*      m_pColorFrameReader(NULL);
@@ -19,10 +19,15 @@ IColorFrameReader*      m_pColorFrameReader(NULL);
 RGBQUAD*                m_pColorRGBX;
 static const int        cColorWidth  = 1920;
 static const int        cColorHeight = 1080;
+static const int        cDepthWidth  = 512;
+static const int        cDepthHeight = 424;
 
 HRESULT InitializeDefaultSensor()
 {
 	HRESULT hr;
+	HRESULT hr_color;
+	HRESULT hr_depth;
+	HRESULT hr_skeleton;
 
 	hr = GetDefaultKinectSensor(&m_pKinectSensor);
 	if (FAILED(hr))
@@ -32,25 +37,29 @@ HRESULT InitializeDefaultSensor()
 
 	if (m_pKinectSensor)
 	{
-		// Initialize the Kinect and get the color reader
+		// Initialize the Kinect and get the color and depth reader
 		IColorFrameSource* pColorFrameSource = NULL;
+		IDepthFrameSource* pDepthFrameSource = NULL;
 
 		hr = m_pKinectSensor->Open();
 
 		if (SUCCEEDED(hr))
 		{
-			hr = m_pKinectSensor->get_ColorFrameSource(&pColorFrameSource);
+			hr_color = m_pKinectSensor->get_ColorFrameSource(&pColorFrameSource);
+			hr_depth = m_pKinectSensor->get_DepthFrameSource(&pDepthFrameSource);
 		}
 
-		if (SUCCEEDED(hr))
+		if (SUCCEEDED(hr_color) && SUCCEEDED(hr_depth))
 		{
-			hr = pColorFrameSource->OpenReader(&m_pColorFrameReader);
+			hr_color = pColorFrameSource->OpenReader(&m_pColorFrameReader);
+			hr_depth = pDepthFrameSource->OpenReader(&m_pDepthFrameReader);
 		}
 
 		SafeRelease(pColorFrameSource);
+		SafeRelease(pDepthFrameSource);
 	}
 
-	if (!m_pKinectSensor || FAILED(hr))
+	if (!m_pKinectSensor || FAILED(hr_color) || FAILED(hr_depth))
 	{
 		cout<<"No ready Kinect found!"<<endl;
 		return E_FAIL;
@@ -59,7 +68,7 @@ HRESULT InitializeDefaultSensor()
 	return hr;
 }
 
-bool Update(IplImage* img)
+bool UpdateColor(IplImage* img)
 {
 	if (!m_pColorFrameReader)
 	{
@@ -151,6 +160,80 @@ bool Update(IplImage* img)
 
 }
 
+void UpdateDepth(IplImage* img)
+{
+	if (!m_pDepthFrameReader)
+	{
+		return;
+	}
+
+	IDepthFrame* pDepthFrame = NULL;
+
+	HRESULT hr = m_pDepthFrameReader->AcquireLatestFrame(&pDepthFrame);
+
+	if (SUCCEEDED(hr))
+	{
+		INT64 nTime = 0;
+		IFrameDescription* pFrameDescription = NULL;
+		int nWidth = 0;
+		int nHeight = 0;
+		USHORT nDepthMinReliableDistance = 0;
+		USHORT nDepthMaxReliableDistance = 0;
+		UINT nBufferSize = 0;
+		UINT16 *pBuffer = NULL;
+
+		hr = pDepthFrame->get_RelativeTime(&nTime);
+
+		if (SUCCEEDED(hr))
+		{
+			hr = pDepthFrame->get_FrameDescription(&pFrameDescription);
+		}
+
+		if (SUCCEEDED(hr))
+		{
+			hr = pFrameDescription->get_Width(&nWidth);
+		}
+
+		if (SUCCEEDED(hr))
+		{
+			hr = pFrameDescription->get_Height(&nHeight);
+		}
+
+		if (SUCCEEDED(hr))
+		{
+			hr = pDepthFrame->get_DepthMinReliableDistance(&nDepthMinReliableDistance);
+		}
+
+		if (SUCCEEDED(hr))
+		{
+			hr = pDepthFrame->get_DepthMaxReliableDistance(&nDepthMaxReliableDistance);
+		}
+
+		if (SUCCEEDED(hr))
+		{
+			hr = pDepthFrame->AccessUnderlyingBuffer(&nBufferSize, &pBuffer);            
+		}
+
+		if (SUCCEEDED(hr))
+		{
+			//ProcessDepth(nTime, pBuffer, nWidth, nHeight, nDepthMinReliableDistance, nDepthMaxReliableDistance);
+			for (int y=0;y<nHeight;y++)
+			{
+				uchar* src_ptr = (uchar*)(img->imageData + y*img->widthStep);
+				for (int x=0;x<nWidth;x++)
+				{
+					(BYTE)src_ptr[x] = (pBuffer[y*nWidth + x]-nDepthMinReliableDistance)*255/nDepthMaxReliableDistance;
+
+				}
+			}
+		}
+
+		SafeRelease(pFrameDescription);
+	}
+
+	SafeRelease(pDepthFrame);
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 
@@ -159,18 +242,48 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	InitializeDefaultSensor();
 
-	IplImage* showImg = cvCreateImage(cvSize(cColorWidth,cColorHeight),8,3);
-	IplImage* showImg_640_480 = cvCreateImage(cvSize(640,480),8,3);
-	cvNamedWindow("show",1);
+	IplImage* showImg_color = cvCreateImage(cvSize(cColorWidth,cColorHeight),8,3);
+	IplImage* showImg_depth = cvCreateImage(cvSize(cDepthWidth,cDepthHeight),8,1);
+	IplImage* showImg_color_640_480 = cvCreateImage(cvSize(640,480),8,3);
+	IplImage* showImg_depth_640_480 = cvCreateImage(cvSize(640,480),8,1);
+	cvNamedWindow("show_color",1);
+	cvNamedWindow("show_depth",1);
 	
 	while (1)
 	{
-		Update(showImg);
-		cvResize(showImg, showImg_640_480);
-		cvShowImage("show",showImg_640_480);
+		UpdateColor(showImg_color);
+		UpdateDepth(showImg_depth);
+		cvResize(showImg_color, showImg_color_640_480);
+		cvResize(showImg_depth,showImg_depth_640_480);
+		cvShowImage("show_color",showImg_color_640_480);
+		cvShowImage("show_depth",showImg_depth);
 		waitKey(10);
 	}
 	
+	cvDestroyWindow("show_color");
+	cvDestroyWindow("show_depth");
+
+
+	//Release
+	cvReleaseImage(&showImg_color);
+	cvReleaseImage(&showImg_depth);
+	cvReleaseImage(&showImg_color_640_480);
+	cvReleaseImage(&showImg_depth_640_480);
+	if (m_pColorRGBX)
+	{
+		delete [] m_pColorRGBX;
+		m_pColorRGBX = NULL;
+	}
+
+	// done with color frame reader
+	SafeRelease(m_pColorFrameReader);
+
+	// close the Kinect Sensor
+	if (m_pKinectSensor)
+	{
+		m_pKinectSensor->Close();
+	}
+	SafeRelease(m_pKinectSensor);
 	return 0;
 }
 
